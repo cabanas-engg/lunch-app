7b92b85d530d139a1c2184d9a424b37bbc9a46e9
Daniel Cabanas <daniel@ddmg.net>
2023-12-26 16:19:38 -0500

    initial API setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import {
+  HttpClientModule,
+  HttpClient
+} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
